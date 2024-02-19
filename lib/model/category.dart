33d58fc3ffa3aa8b742347677943dace8a5e14ee
taufiqelahi <taufiqelahiiceiu@gmail.com>
2024-02-19 09:09:23 +0600

    added dummy data

--- lib/model/category.dart
+++ lib/model/category.dart
@@ -5,5 +5,5 @@ class Category {
   final String title;
   final Color color;
 
-  Category({required this.id, required this.title, required this.color});
+  const Category({required this.id, required this.title, required this.color});
 }
